e075f1ca0fdafd353f11ebbcbf59342991d4e435
Paige Liu <liupeirong@gmail.com>
2014-08-12 12:02:27 -0700

    fixed graph data

--- CDMetric2/CDMetric2/Scripts/App/metricControllers.js
+++ CDMetric2/CDMetric2/Scripts/App/metricControllers.js
@@ -35,11 +35,15 @@ metricControllers.controller('rolloutSummaryController', ['$scope', 'RolloutSumm
 }]);
 
 metricControllers.controller('rolloutChartsController', ['$scope', 'RolloutDetails', function ($scope, RolloutDetails) {
-    var lastChangeNumber = 0;
-    var series_web = {};
-    var series_caption = {};
-    var series_mm = {};
-    var series_feeds = {};
+    var stages_web = {};
+    var stages_caption = {};
+    var stages_mm = {};
+    var stages_feeds = {};
+
+    var rollouts_web = {};
+    var rollouts_caption = {};
+    var rollouts_mm = {};
+    var rollouts_feeds = {};
     
     $scope.data_web = {series: [], data: []};
     $scope.data_caption = { series: [], data: [] };
@@ -49,62 +53,50 @@ metricControllers.controller('rolloutChartsController', ['$scope', 'RolloutDetai
     RolloutDetails.query(function (data) {
         angular.forEach(data, function (metric) {
             var duration = (metric.DurationInMin / 60);
+            var changeNumber = metric.ChangeNumber.toString();
 
-            if (metric.ChangeNumber != lastChangeNumber) {
-                var point = {};
-                point.x = metric.ChangeNumber.toString();
-                point.y = [];
-                point.y.push(duration);
-                if (metric.RolloutName.indexOf('MMServe') > -1) {
-                    if (!(metric.StageName in series_mm))
-                        series_mm[metric.StageName] = 0;
-                    $scope.data_mm.data.push(point);
-                } else if (metric.RolloutName.indexOf('caption') > -1) {
-                    if (!(metric.StageName in series_caption))
-                        series_caption[metric.StageName] = 0;
-                    $scope.data_caption.data.push(point);
-                } else if (metric.RolloutName.indexOf('Feeds') > -1) {
-                    if (!(metric.StageName in series_feeds))
-                        series_feeds[metric.StageName] = 0;
-                    $scope.data_feeds.data.push(point);
-                } else {
-                    if (!(metric.StageName in series_web))
-                        series_web[metric.StageName] = 0;
-                    $scope.data_web.data.push(point);
-                }
-                lastChangeNumber = metric.ChangeNumber;
+            //create stages data structure like this: 
+            //{"stage1": {"changeNumber1": duration,
+            //            "changeNumber2": duration
+            //           },
+            // "stage2": {"changeNumber2": duration,
+            //            "changeNumber1": duration
+            //           };
+            //}
+            //create rollouts as an associative array like this, used as a dictionary keys:
+            //{ "changeNumber1":0,
+            //  "changeNumber2":0
+            //}
+            if (metric.RolloutName.indexOf('MMServe') > -1) {
+                if (!(metric.StageName in stages_mm))
+                    stages_mm[metric.StageName] = {};
+                if (!(changeNumber in rollouts_mm))
+                    rollouts_mm[changeNumber] = 0;
+                stages_mm[metric.StageName][changeNumber] = duration;
+            } else if (metric.RolloutName.indexOf('caption') > -1) {
+                if (!(metric.StageName in stages_caption))
+                    stages_caption[metric.StageName] = {};
+                if (!(changeNumber in rollouts_caption))
+                    rollouts_caption[changeNumber] = 0;
+                stages_caption[metric.StageName][changeNumber] = duration;
+            } else if (metric.RolloutName.indexOf('Feeds') > -1) {
+                if (!(metric.StageName in stages_feeds))
+                    stages_feeds[metric.StageName] = {};
+                if (!(changeNumber in rollouts_feeds))
+                    rollouts_feeds[changeNumber] = 0;
+                stages_feeds[metric.StageName][changeNumber] = duration;
             } else {
-                if (metric.RolloutName.indexOf('MMServe') > -1) {
-                    if (!(metric.StageName in series_mm))
-                        series_mm[metric.StageName] = 0;
-                    $scope.data_mm.data[$scope.data_mm.data.length - 1].y.push(duration);
-                } else if (metric.RolloutName.indexOf('caption') > -1) {
-                    if (!(metric.StageName in series_caption))
-                        series_caption[metric.StageName] = 0;
-                    $scope.data_caption.data[$scope.data_caption.data.length - 1].y.push(duration);
-                } else if (metric.RolloutName.indexOf('Feeds') > -1) {
-                    if (!(metric.StageName in series_feeds))
-                        series_feeds[metric.StageName] = 0;
-                    $scope.data_feeds.data[$scope.data_feeds.data.length - 1].y.push(duration);
-                } else {
-                    if (!(metric.StageName in series_web))
-                        series_web[metric.StageName] = 0;
-                    $scope.data_web.data[$scope.data_web.data.length - 1].y.push(duration);
-                }
+                if (!(metric.StageName in stages_web))
+                    stages_web[metric.StageName] = {};
+                if (!(changeNumber in rollouts_web))
+                    rollouts_web[changeNumber] = 0;
+                stages_web[metric.StageName][changeNumber] = duration;
             }
         });
-        for (var key in series_web) {
-            $scope.data_web.series.push(key);
-        }
-        for (var key in series_caption) {
-            $scope.data_caption.series.push(key);
-        }
-        for (var key in series_mm) {
-            $scope.data_mm.series.push(key);
-        }
-        for (var key in series_feeds) {
-            $scope.data_feeds.series.push(key);
-        }
+        db2graph(stages_web, rollouts_web, $scope.data_web);
+        db2graph(stages_caption, rollouts_caption, $scope.data_caption);
+        db2graph(stages_mm, rollouts_mm, $scope.data_mm);
+        db2graph(stages_feeds, rollouts_feeds, $scope.data_feeds);
     });
 
     $scope.config_web = {
@@ -156,3 +148,39 @@ metricControllers.controller('rolloutChartsController', ['$scope', 'RolloutDetai
         }
     };
 }]);
+
+//convert the stages and rollouts data structure to $scope.data_ data structure like this:
+//{
+//  series: ["stage1", "stage2"...],
+//  data:[
+//         {x: "changeNumber1", y: [stage1Duration, stage2Duration...]},
+//         {x: "changeNumber2", y: [stage2Duration, stage2Duration...]}
+//       ]
+//}
+function db2graph(curstages, currollouts, scopedata) {
+    for (var key in curstages) {
+        scopedata.series.push(key);
+    }
+    scopedata.series.sort();
+    var rollouts = [];
+    for (var key in currollouts) {
+        rollouts.push(key);
+    }
+    rollouts.sort();
+
+    for (var k = 0; k < rollouts.length; ++k) {
+        var rollout = rollouts[k];
+        var point = {};
+        point.x = rollout;
+        point.y = [];
+        var stages = scopedata.series.length;
+        for (var i = 0; i < stages; ++i) {
+            var stage = scopedata.series[i];
+            var rolloutsInStage = curstages[stage];
+            var duration = (rollout in rolloutsInStage) ? rolloutsInStage[rollout] : 0;
+            if (duration == null) duration = 0;
+            point.y.push(duration);
+        }
+        scopedata.data.push(point);
+    }
+};
